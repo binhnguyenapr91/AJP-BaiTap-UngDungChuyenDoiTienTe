c9a144f4a0063fd5d30c0ce6b3dbb82dbb0b5a9b
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-03-19 20:24:33 +0700

    init commit

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 public class CurrencyConverter {
+    static final double RATE = 23000;
+
     public static void main(String[] args) {
         double vnd;
         double usd;
-        final double RATE = 23000;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter USD:");
         usd = scanner.nextDouble();
         vnd = usd * RATE;
         System.out.println("VND: " + vnd);
-
     }
 }
